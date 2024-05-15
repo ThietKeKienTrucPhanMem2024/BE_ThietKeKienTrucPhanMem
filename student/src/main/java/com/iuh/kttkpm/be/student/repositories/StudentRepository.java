b41e73a1b8b070c5be04b3ef4a06eafe12e257c5
NguyenDucChien <cnchien@gmail.com>
2024-05-15 22:06:39 +0700

    new cource
    new cource

--- student/src/main/java/com/iuh/kttkpm/be/student/repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iuh.kttkpm.be.student.repositories;
-
-import com.iuh.kttkpm.be.student.models.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, String> {
-}
