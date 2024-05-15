383a1a812f8eae5eea228500e58db823d2d4d325
NguyenDucChien <cnchien@gmail.com>
2024-05-15 21:23:37 +0700

    new cource
    new cource

---
+++ look_up_points/src/test/java/com/iuh/kttkpm/be/look_up_points/LookUpPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.kttkpm.be.look_up_points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookUpPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
